b328f80f3b43538964a7c48d845b9c8774f63825
Lukasos Studios <baltagmatei8@gmail.com>
2026-08-01 14:58:58 +0300

    Add PlayerSession class for managing player state

---
+++ src/main/java/com/lukasosstudios/authmod/PlayerSession.java
@@ -0,0 +1,24 @@
+package com.lukasosstudios.authmod;
+
+import net.minecraft.world.phys.Vec3;
+
+public class PlayerSession {
+    public AuthState state;
+    public Vec3 frozenPosition;
+    public float frozenYaw;
+    public float frozenPitch;
+    public int ticksUntilKick;
+    public int failedAttempts = 0;
+
+    public PlayerSession(AuthState state, Vec3 frozenPosition, float frozenYaw, float frozenPitch, int ticksUntilKick) {
+        this.state = state;
+        this.frozenPosition = frozenPosition;
+        this.frozenYaw = frozenYaw;
+        this.frozenPitch = frozenPitch;
+        this.ticksUntilKick = ticksUntilKick;
+    }
+
+    public boolean isAuthenticated() {
+        return state == AuthState.AUTHENTICATED;
+    }
+}
